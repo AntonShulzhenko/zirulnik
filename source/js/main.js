3288f93135c36ffe78d6694302117cde0de5bb2a
AntonShulzhenko <shulzhenkomailbox@gmail.com>
2016-09-04 20:04:04 +0300

    nav + header

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,31 @@
-var modules = 'aaa';
+$( document ).ready(function() {
 
-var test = 'fhjfhjfh';
+  function toggleMainNav() {
+    var nav     = $('.main-nav');
+    var navBtn  = $('.main-nav__close');
+    var menuBtn = $('.header__menu-btn');
+
+    navBtn.on('click', function() {
+      nav.slideUp();
+    });
+
+    menuBtn.on('click', function() {
+      nav.slideDown();
+    });
+  }
+
+  toggleMainNav();
+
+  // ADD SLIDEDOWN ANIMATION TO DROPDOWN //
+  $('.dropdown').on('show.bs.dropdown', function (e) {
+      $(this).find('.dropdown-menu').first().stop(true, true).addClass('animated');
+      $(this).find('.dropdown-toggle').addClass('animated');
+  });
+
+  // ADD SLIDEUP ANIMATION TO DROPDOWN //
+  $('.dropdown').on('hide.bs.dropdown', function (e) {
+      $(this).find('.dropdown-menu').first().stop(true, true).removeClass('animated');
+      $(this).find('.dropdown-toggle').removeClass('animated');
+  });
+
+});
