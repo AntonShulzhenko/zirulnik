c8815f87bb429424c18344ea846606665b4c6c90
Anton Shulzhenko <shulzhenkomailbox@gmail.com>
2016-09-06 21:43:03 +0300

    footer

--- source/js/main.js
+++ source/js/main.js
@@ -1,16 +1,23 @@
 $( document ).ready(function() {
 
   function toggleMainNav() {
+    var main    = $('.main');
     var nav     = $('.main-nav');
     var navBtn  = $('.main-nav__close');
     var menuBtn = $('.header__menu-btn');
 
-    navBtn.on('click', function() {
-      nav.slideUp();
+    menuBtn.on('click', function() {
+      main.addClass('trans');
+      setTimeout(function() {
+        nav.addClass('show');
+      }, 300);
     });
 
-    menuBtn.on('click', function() {
-      nav.slideDown();
+    navBtn.on('click', function() {
+      nav.removeClass('show');
+      setTimeout(function() {
+        main.removeClass('trans');
+      }, 500);
     });
   }
 
